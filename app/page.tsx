cafb081fcff447a14a3f0dabcca3f0162ba8ce5c
Arrowsengoku <yudam1217@gmail.com>
2025-02-27 10:15:16 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -113,7 +113,7 @@ export default function Home() {
               className="text-center"
             >
               <Image
-                src="/home/project/public/controller.PNG"
+                src="public/controller.PNG"
                 alt="T2411 Controller"
                 width={1200}
                 height={675}
