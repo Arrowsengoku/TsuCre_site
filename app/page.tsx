9a86e569d0183dd862e8b57d870308f5e0e66c9e
Arrowsengoku <yudam1217@gmail.com>
2025-02-28 13:20:29 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email }),
+        body: JSON.stringify({ email: userEmail }),
       });
 
       if (!response.ok) throw new Error('API error');
