90dec1b52e9e53913fc4c9142ae955881946d16b
Arrowsengoku <yudam1217@gmail.com>
2025-02-28 16:52:57 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // App Routerを使用していることを明示
   output: 'export',
   images: {
     unoptimized: true,
   },
   experimental: {
     esmExternals: false, // モジュールの解決を調整
+  },
+    async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/:path*", // APIルートを適切に解釈
+      },
+    ];
   },
   webpack: (config) => {
     config.externals = config.externals || [];
